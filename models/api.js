8943b664c3f728ec0ddfcd0b2f1d4dfd8d272e92
pishifat <31551350+pishifat@users.noreply.github.com>
2019-02-28 23:43:28 -0800

    a

--- models/api.js
+++ models/api.js
@@ -125,7 +125,7 @@ async function isLoggedIn(req, res, next) {
         const u = await users.service.query({ _id: req.session.mongoId });
         
         // If hidden, shouldn't be able to do anything
-        if (!u || u.group == 'hidden' || u.group != 'admin') {
+        if (!u || u.group == 'hidden') {
             return res.redirect('/');
         }
 
