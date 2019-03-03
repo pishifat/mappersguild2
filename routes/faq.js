bd534010cbe517d904053646ec653868bff2d046
milaneyia <39941577+milaneyia@users.noreply.github.com>
2019-03-02 23:22:55 -0300

    ops

--- routes/faq.js
+++ routes/faq.js
@@ -5,14 +5,14 @@ var router = express.Router();
 
 /* GET faq */
 router.get('/', async (req, res, next) => {
-    const u = await users.service.query({ _id: req.session.mongoId });
+    const u = await users.service.query({ _id: req.session.mongoId }, [{ populate: 'currentParty', display: 'name' }]);
     if (u) {
         res.render('faq', {
             title: 'Frequently Asked Questions',
             isFaq: true,
             loggedInAs: u.osuId,
-            userTotalPoints: res.locals.userRequest.totalPoints,
-            userParty: res.locals.userRequest.currentParty ? res.locals.userRequest.currentParty.name : null,
+            userTotalPoints: u.userRequest.totalPoints,
+            userParty: u.userRequest.currentParty ? u.userRequest.currentParty.name : null,
         });
     } else {
         res.render('faq', { title: 'Frequently Asked Questions', isFaq: true });
