6e4ee81ecf84ef9f721066337dfe7d464d6e6d60
milaneyia <39941577+milaneyia@users.noreply.github.com>
2019-04-04 23:07:56 -0300

    ok

--- QAT/src/components/evaluations/EvalCard.vue
+++ QAT/src/components/evaluations/EvalCard.vue
@@ -1,13 +1,9 @@
 <template>
-<div class='col-md-2 my-2'>
+<div class='col-md-12 my-2'>
     <div class="card custom-bg-dark border-outline" style="height: 100%">
-        <div class='card-body notification-card-spacing mx-1'>
-            <p class='card-text text-shadow'>
-                <i v-if="applicant.mode == 'osu'" class="far fa-circle"></i>
-                <i v-else-if="applicant.mode == 'taiko'" class="fas fa-drum"></i>
-                <i v-else-if="applicant.mode == 'catch'" class="fas fa-apple-alt"></i>
-                <i v-else-if="applicant.mode == 'mania'" class="fas fa-stream"></i>
-                <a :href="'https://osu.ppy.sh/users/' + applicant.osuId">{{applicant.username}}</a> 
+        <div class='card-body notification-card-spacing'>
+            <p class='card-text text-shadow small'>
+                {{applicant.username}}
             </p>
         </div>
         <div class="card-footer notification-card-spacing mx-2">
@@ -22,11 +18,7 @@ export default {
     name: 'eval-card',
     props: ['applicant'],
     methods: {
-        createDeadline: function(date){
-            date = new Date(date);
-            date = new Date(date.setDate (date.getDate() + 7)).toString().slice(4,10);
-            return date;
-        }
+        
     },
 }
 </script>
