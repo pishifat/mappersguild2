d490f618d1d97f447f0dbbd6752b197dec67fa57
milaneyia <39941577+milaneyia@users.noreply.github.com>
2019-04-04 23:00:55 -0300

    Revert "populating the populate"
    This reverts commit 8cd4b827add5572a77e66a504facdce10801346a.

--- QAT/models/evalRound.js
+++ QAT/models/evalRound.js
@@ -33,9 +33,11 @@ class EvalRoundService
                 const p = populate[i];
                 
                 if (p.innerPopulate) {
-                    query.populate({ path: p.innerPopulate, model: p.model, populate: p.populate });
+                    query.populate({ path: p.innerPopulate, populate: p.populate }, 
+                        p.model == 'QatUser' ? users.QatUser : evals.Evaluation);
                 } else {
-                    query.populate(p.populate, p.display, p.model);
+                    query.populate(p.populate, p.display, 
+                        p.model == 'QatUser' ? users.QatUser : evals.Evaluation);
                 }
             }
         }
