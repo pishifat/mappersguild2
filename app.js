4a17a8364e1c67b642ea6d09fbbd90f9df99812b
milaneyia <39941577+milaneyia@users.noreply.github.com>
2019-04-04 23:01:08 -0300

    Revert "oops"
    This reverts commit 549dcd0da33dbbf6e25d1f9e84bd13ebaf189765.

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ const adminsRouter = require('./routes/admin');
 //qat
 const bnAppRouter = require('./QAT/routes/bnApp');
 const appEvalRouter = require('./QAT/routes/appEval');
-const qatUsersRouter = require('./QAT/routes/qatUsers');
 
 const logs = require('./models/log');
 
@@ -84,7 +83,6 @@ qatdb.once('open', function() {
 //qat routes
 app.use('/qat', bnAppRouter);
 app.use('/qat/appEval', appEvalRouter);
-app.use('/qat/qatusers', qatUsersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
