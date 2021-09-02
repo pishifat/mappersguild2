4bf06539bd27ed023b2edbce1ce38888dafe3b86
pishifat <pishifat@gmail.com>
2021-09-02 16:04:38 -0700

    dist2

---
+++ dist/public/assets/NotFound.38cca82c.js
@@ -0,0 +1,1 @@
+import{d as o,o as t,b as e}from"./vendor.cb33bf1d.js";var n=o({name:"NotFound"});const r={class:"text-center"};n.render=function(o,n,a,s,d,c){return t(),e("h3",r," Not found :( ")};export{n as default};
