adb155ac80bae92ab2495f4e2964d5756f4ef3d4
pishifat <pishifat@gmail.com>
2022-05-09 12:21:35 -0700

    packages and dist of nothing

---
+++ dist/public/assets/Error.aa0979a2.js
@@ -0,0 +1,1 @@
+import{d as a,o as r,b as e,f as n}from"./vendor.7edd7468.js";var s=a({name:"Error"});const o={class:"container text-center mb-2"},t=[n("div",{class:"card"},[n("div",{class:"card-header"}," Something went wrong! "),n("div",{class:"card-body"}," Contact pishifat if you continue having problems ")],-1)];s.render=function(a,n,s,d,c,i){return r(),e("div",o,t)};export{s as default};
---
+++ dist/public/assets/NotFound.080a2865.js
@@ -0,0 +1,1 @@
+import{d as e,o,b as t}from"./vendor.7edd7468.js";var n=e({name:"NotFound"});const r={class:"text-center"};n.render=function(e,n,a,d,s,u){return o(),t("h3",r," Not found :( ")};export{n as default};
