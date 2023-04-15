322aa23fca058a1a4d183fa3fe6f289ed4305683
pishifat <31551350+pishifat@users.noreply.github.com>
2023-04-15 13:12:52 -0700

    dist

--- dist/api/routes/contests/listing.js
+++ dist/api/routes/contests/listing.js
@@ -37,7 +37,7 @@ const defaultContestPopulate = [
             {
                 path: 'judgings',
                 populate: {
-                    path: 'judge',
+                    path: 'judge judgingScores',
                 },
             },
         ],
