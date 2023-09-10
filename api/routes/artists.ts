4ff86df1d3bcae36f79f2e9a0f255d3f4f1cc523
pishifat <31551350+pishifat@users.noreply.github.com>
2023-09-09 17:36:00 -0700

    artist page less lines

--- api/routes/artists.ts
+++ api/routes/artists.ts
@@ -59,148 +59,105 @@ artistsRouter.post('/create', async (req, res) => {
 
 /* POST toggle isContacted */
 artistsRouter.post('/toggleIsContacted/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { isContacted: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { isContacted: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle isResponded */
 artistsRouter.post('/toggleisResponded/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { isResponded: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { isResponded: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle isRejected */
 artistsRouter.post('/toggleisRejected/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { isRejected: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { isRejected: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle contractSent */
 artistsRouter.post('/toggleContractSent/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { contractSent: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { contractSent: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle artistSigned */
 artistsRouter.post('/toggleArtistSigned/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { artistSigned: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { artistSigned: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle ppyPaid */
 artistsRouter.post('/togglePpyPaid/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { ppyPaid: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { ppyPaid: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle ppySigned */
 artistsRouter.post('/togglePpySigned/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { ppySigned: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { ppySigned: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle songsTimed */
 artistsRouter.post('/toggleSongsTimed/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { songsTimed: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { songsTimed: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle songsReceived */
 artistsRouter.post('/toggleSongsReceived/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { songsReceived: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { songsReceived: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle assetsReceived */
 artistsRouter.post('/toggleAssetsReceived/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { assetsReceived: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { assetsReceived: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle hasRankedMaps */
 artistsRouter.post('/toggleHasRankedMaps/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { hasRankedMaps: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { hasRankedMaps: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle isCommission */
 artistsRouter.post('/toggleIsCommission/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { isCommission: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { isCommission: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle hasNewSongs */
 artistsRouter.post('/toggleHasNewSongs/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { hasNewSongs: req.body.value });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { hasNewSongs: req.body.value }).defaultPopulate());
 });
 
 /* POST toggle isUpToDate */
 artistsRouter.post('/toggleIsUpToDate/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, {
-        isUpToDate: req.body.value,
-        contractSent: false,
-        artistSigned: false,
-        ppyPaid: false,
-        ppySigned: false,
-        songsReceived: false,
-        songsTimed: false,
-        projectedRelease: undefined,
-    });
+    let a = await FeaturedArtistModel.findById(req.params.id);
 
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
+    if (!req.body.value && !a.isCommission && !a?.hasNewSongs) {
+        return res.json({ error: `Can't be not-up-to-date without commission or pending songs!` });
+    }
+
+    a = await FeaturedArtistModel
+        .findByIdAndUpdate(req.params.id, {
+            isUpToDate: req.body.value,
+            contractSent: false,
+            artistSigned: false,
+            ppyPaid: false,
+            ppySigned: false,
+            songsReceived: false,
+            songsTimed: false,
+            projectedRelease: undefined,
+        }).defaultPopulate();
 
     res.json(a);
 });
 
 /* POST update projectedRelease */
 artistsRouter.post('/updateProjectedRelease/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { projectedRelease: req.body.date });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { projectedRelease: req.body.date }).defaultPopulate());
 });
 
 /* POST update projectedRelease */
 artistsRouter.post('/updateLastContacted/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { lastContacted: req.body.date });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { lastContacted: req.body.date }).defaultPopulate());
 });
 
 /* POST update notes */
 artistsRouter.post('/updateNotes/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { notes: req.body.notes });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, { notes: req.body.notes }).defaultPopulate());
 });
 
 /* POST update showcase mappers */
@@ -236,7 +193,7 @@ artistsRouter.post('/updateShowcaseMappers/:id', async (req, res) => {
 
 /* POST reset progress */
 artistsRouter.post('/reset/:id', async (req, res) => {
-    let a = await FeaturedArtistModel.findByIdAndUpdate(req.params.id, {
+    res.json(await FeaturedArtistModel.findByIdAndUpdate(req.params.id, {
         isResponded: false,
         isRejected: false,
         contractSent: false,
@@ -248,17 +205,12 @@ artistsRouter.post('/reset/:id', async (req, res) => {
         hasRankedMaps: false,
         isUpToDate: false,
         projectedRelease: undefined,
-    });
-    a = await FeaturedArtistModel.findById(req.params.id).defaultPopulate();
-
-    res.json(a);
+    }).defaultPopulate());
 });
 
 /* POST delete artist */
 artistsRouter.post('/deleteArtist/:id', async (req, res) => {
-    const a = await FeaturedArtistModel.findByIdAndRemove(req.params.id);
-
-    res.json(a);
+    res.json(await FeaturedArtistModel.findByIdAndRemove(req.params.id));
 });
 
 /* POST set all contacted but unreplied artists as rejected */
@@ -377,72 +329,64 @@ artistsRouter.post('/osuBeatmaps/updateIsLicensed/:id', async (req, res) => {
         .findById(req.params.id)
         .orFail();
 
-    const newOsuBeatmap = await OsuBeatmapModel
+    res.json(await OsuBeatmapModel
         .findByIdAndUpdate(req.params.id, { isLicensed: !osuBeatmap.isLicensed })
         .populate(defaultOsuBeatmapPopulate)
-        .orFail();
-
-    res.json(newOsuBeatmap);
+        .orFail());
 });
 
 /* POST update osuBeatmap comment */
 artistsRouter.post('/osuBeatmaps/updateComment/:id', async (req, res) => {
-    const newOsuBeatmap = await OsuBeatmapModel
+    res.json(await OsuBeatmapModel
         .findByIdAndUpdate(req.params.id, { comment: req.body.comment })
         .populate(defaultOsuBeatmapPopulate)
-        .orFail();
-
-    res.json(newOsuBeatmap);
+        .orFail()
+    );
 });
 
 /* POST add FA to song */
 artistsRouter.post('/osuBeatmaps/addArtistToOsuBeatmap/:id', async (req, res) => {
-    const newOsuBeatmap = await OsuBeatmapModel
+    res.json(await OsuBeatmapModel
         .findByIdAndUpdate(req.params.id, { $push: { featuredArtists: req.body.artistId } })
         .populate(defaultOsuBeatmapPopulate)
-        .orFail();
-
-    res.json(newOsuBeatmap);
+        .orFail()
+    );
 });
 
 /* POST remove FA from song */
 artistsRouter.post('/osuBeatmaps/removeArtistFromOsuBeatmap/:id', async (req, res) => {
-    const newOsuBeatmap = await OsuBeatmapModel
+    res.json(await OsuBeatmapModel
         .findByIdAndUpdate(req.params.id, { $pull: { featuredArtists: req.body.artistId } })
         .populate(defaultOsuBeatmapPopulate)
-        .orFail();
-
-    res.json(newOsuBeatmap);
+        .orFail()
+    );
 });
 
 /* POST add administrator to song */
 artistsRouter.post('/osuBeatmaps/addAdministratorToOsuBeatmap/:id', async (req, res) => {
-    const newOsuBeatmap = await OsuBeatmapModel
+    res.json(await OsuBeatmapModel
         .findByIdAndUpdate(req.params.id, { $push: { administrators: req.body.administrator } })
         .populate(defaultOsuBeatmapPopulate)
-        .orFail();
-
-    res.json(newOsuBeatmap);
+        .orFail()
+    );
 });
 
 /* POST remove administrator from song */
 artistsRouter.post('/osuBeatmaps/removeAdministratorFromOsuBeatmap/:id', async (req, res) => {
-    const newOsuBeatmap = await OsuBeatmapModel
+    res.json(await OsuBeatmapModel
         .findByIdAndUpdate(req.params.id, { $pull: { administrators: req.body.administrator } })
         .populate(defaultOsuBeatmapPopulate)
-        .orFail();
-
-    res.json(newOsuBeatmap);
+        .orFail()
+    );
 });
 
 /* POST update lastChecked */
 artistsRouter.post('/osuBeatmaps/updateLastChecked/:id', async (req, res) => {
-    const newOsuBeatmap = await OsuBeatmapModel
+    res.json(await OsuBeatmapModel
         .findByIdAndUpdate(req.params.id, { lastChecked: req.body.date })
         .populate(defaultOsuBeatmapPopulate)
-        .orFail();
-
-    res.json(newOsuBeatmap);
+        .orFail()
+    );
 });
 
 export default artistsRouter;
