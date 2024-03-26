04b74349424beec589ec7d920e09b1be058276bb
pishifat <31551350+pishifat@users.noreply.github.com>
2024-03-25 18:31:43 -0700

    probably nothign 2

--- api/routes/contests/judging.ts
+++ api/routes/contests/judging.ts
@@ -120,7 +120,7 @@ judgingRouter.post('/save', isJudge, async (req, res) => {
         return res.json({ error: 'Invalid criteria' });
     }
 
-    if (score > criteria.maxScore) {
+    if (score > criteria.maxScore && (!comment || !comment.length)) {
         return res.json({ error: 'Score is higher than expected' });
     }
 
