5b285fc9c7566376dfd2765c430136021a17ccf1
pishifat <31551350+pishifat@users.noreply.github.com>
2024-04-09 14:01:51 -0700

    dist

--- dist/api/helpers/helpers.js
+++ dist/api/helpers/helpers.js
@@ -205,7 +205,8 @@ async function setBeatmapStatusRanked(id, bmInfo) {
         // user stats text
         let statsText = '';
         if (typeof newPoints === 'number') {
-            statsText += `\n\n${gdUsers.length ? `- [**${beatmap.host.username}**](https://osu.ppy.sh/users/${beatmap.host.osuId}): ` : ''}${oldPoints} pts → **${newPoints} pts** (+${newPoints - oldPoints} pts)`;
+            const pointsDifference = Math.round((newPoints - oldPoints) * 10) / 10;
+            statsText += `\n\n${gdUsers.length ? `- [**${beatmap.host.username}**](https://osu.ppy.sh/users/${beatmap.host.osuId}): ` : ''}${oldPoints} pts → **${newPoints} pts** (+${pointsDifference} pts)`;
             if (newPoints < 2500) {
                 const pointsLeft = Math.round((ranks[getRankFromPoints(newPoints).rank + 1].value - newPoints) * 10) / 10;
                 statsText += `\n${pointsLeft} points until **${ranks[getRankFromPoints(newPoints).rank + 1].name}** rank`;
@@ -213,7 +214,8 @@ async function setBeatmapStatusRanked(id, bmInfo) {
             if (gdUsers.length) {
                 statsText += '\n';
                 for (const gder of gderInfo) {
-                    statsText += `\n- [**${gder.username}**](https://osu.ppy.sh/users/${gder.osuId}): ${gder.oldPoints} pts → **${gder.newPoints} pts** (+${gder.newPoints - gder.oldPoints} pts)`;
+                    const gderPointsDifference = Math.round((gder.newPoints - gder.oldPoints) * 10) / 10;
+                    statsText += `\n- [**${gder.username}**](https://osu.ppy.sh/users/${gder.osuId}): ${gder.oldPoints} pts → **${gder.newPoints} pts** (+${gderPointsDifference} pts)`;
                 }
             }
         }
