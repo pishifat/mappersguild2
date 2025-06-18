d79742dcedc4ac4a2328b83d8fbf536895423c01
pishifat <31551350+pishifat@users.noreply.github.com>
2025-06-17 20:52:41 -0700

    dist

--- dist/api/routes/contests/listing.js
+++ dist/api/routes/contests/listing.js
@@ -1062,7 +1062,7 @@ listingRouter.post('/:id/addJudgingsFromCsv', middlewares_2.isContestCreator, mi
         const line = rawLine.split('\t');
         const submission = contest.submissions.find(s => s.creator.osuId == parseInt(line[0]));
         console.log(submission?.creator.username);
-        const judgeId = '62b1a32becf25121cfe756df';
+        const judgeId = '5c6e6db559d335001922e6df';
         const judging = new judging_1.JudgingModel();
         /** @ts-ignore */
         judging.judge = judgeId;
@@ -1077,13 +1077,13 @@ listingRouter.post('/:id/addJudgingsFromCsv', middlewares_2.isContestCreator, mi
                     criteriaId = '5e2ec4c071f7f84b0a73ca71';
                     break;
                 case 2:
-                    criteriaId = '62a4ce9decf25121cfe649e2';
+                    criteriaId = '65ffc36af86998bffceec0e5';
                     break;
                 case 3:
-                    criteriaId = '62a4ceabecf25121cfe649ed';
+                    criteriaId = '65ffc374f86998bffceec0f5';
                     break;
                 case 4:
-                    criteriaId = '62a4cee7ecf25121cfe649f8';
+                    criteriaId = '65ffc388f86998bffceec105';
                     break;
                 default:
                     console.log('fucked');
@@ -1095,7 +1095,7 @@ listingRouter.post('/:id/addJudgingsFromCsv', middlewares_2.isContestCreator, mi
             let score = 0;
             console.log(i);
             if (i == 1) {
-                const finalScoreInput = scoreInput.replace(/\/\//g, '\n');
+                const finalScoreInput = scoreInput.replace(/\\n\\n/g, '\n\n');
                 comment = finalScoreInput;
             }
             else {
--- dist/api/routes/interOp.js
+++ dist/api/routes/interOp.js
@@ -12,13 +12,34 @@ const interOpRouter = express_1.default.Router();
 interOpRouter.use((req, res, next) => {
     const secret = req.header('secret');
     const username = req.header('username');
-    if (!secret || !username || config_json_1.default.interOpAccess[username].secret !== secret) {
-        return res.status(401).send('Invalid key');
+    if (!secret || !username) {
+        return res.status(401).json({ error: 'Missing secret or username headers' });
+    }
+    const userConfig = config_json_1.default.interOpAccess[username];
+    if (!userConfig || userConfig.secret !== secret) {
+        return res.status(401).json({ error: 'Invalid credentials' });
     }
     return next();
 });
 /* GET users */
 interOpRouter.get('/users', async (_, res) => {
     res.json(await user_1.UserModel.find({ group: user_2.UserGroup.Admin }));
+});
+/* GET user by osuId */
+interOpRouter.get('/user/:id', async (req, res) => {
+    const osuId = parseInt(req.params.id, 10);
+    if (isNaN(osuId)) {
+        return res.status(400).json({ error: 'Invalid osuId provided' });
+    }
+    try {
+        const user = await user_1.UserModel.findOne({ osuId });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 exports.default = interOpRouter;
