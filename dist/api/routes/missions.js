2af468cc9df0e632c24b14994757593fd407ef23
pishifat <31551350+pishifat@users.noreply.github.com>
2026-03-15 12:00:12 -0700

    dist

--- dist/api/routes/missions.js
+++ dist/api/routes/missions.js
@@ -515,7 +515,7 @@ missionsRouter.post('/:missionId/findShowcaseMissionSongByTag', isEditable, asyn
         await (0, discordApi_1.devWebhookPost)([{
                 title: `genre showcase mission song rerolled`,
                 color: discordApi_1.webhookColors.lightGreen,
-                description: `[**${user.username}**](https://osu.ppy.sh/users/${user.osuId}) rerolled to **${newSong.artist} - ${newSong.title}** for **${mission.name}** priority quest`,
+                description: `[**${user.username}**](https://osu.ppy.sh/users/${user.osuId}) rerolled to **${newSong.artist} - ${newSong.title}** for **${mission.name}** priority quest (${rerollCost} points)`,
             }]);
         log_1.LogModel.generate(req.session?.mongoId, `rerolled genre showcase mission song`, log_2.LogCategory.Mission);
     }
